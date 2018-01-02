7ced2976df2d4fad0f80d05430aadd1010260857
mickhacks <mickbenjaminmulder@gmail.com>
2018-01-02 13:33:38 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,8 +6,8 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.channel.send('PONG!');
+    if (message.content === 'poef') {
+    	message.channel.send('!purge 5');
   	}
 });
 
