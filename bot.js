61627dd8d225309d01fb0f4eb0b9411b755d2976
mickhacks <mickbenjaminmulder@gmail.com>
2018-01-06 10:45:55 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const prefix = '~'; // This is the prefix, you can change it to whatever you want.
+
 
 client.on('ready', () => {
     console.log('I am ready!');
