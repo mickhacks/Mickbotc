7285a9d36514931ccbf95d3d1a0c8672d22a7b55
mickhacks <mickbenjaminmulder@gmail.com>
2018-01-06 10:49:52 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,5 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = '.'; // This is the prefix, you can change it to whatever you want.
-
 
 client.on('ready', () => {
     console.log('I am ready!');
@@ -17,8 +15,6 @@ client.on('message', message => {
     if (message.content === 'bing') {
     	message.reply('BONG!');
   	}
-    
-    
 });
 
 // THIS  MUST  BE  THIS  WAY
