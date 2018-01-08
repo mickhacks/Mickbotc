0f84ab6270faa2a1cd58bd2a9ddcbf95b82ffb44
mickhacks <mickbenjaminmulder@gmail.com>
2018-01-08 16:03:48 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,17 +3,75 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
     console.log('I am ready!');
+    
+     let msg = message.content.toUpperCase(); 
+     let sender = message.author; 
+    
 });
 
 client.on('message', message => {
-    if (message.content === 'D') {
-    	message.channel.send('@Bananie31#3044 Nu kan je wat game-vrienden krijgen  ;)');
+    if (message.content === 'tyfus') {
+    	message.channel.send('Niet schelden godverdomme');
   	}
 });
 
 client.on('message', message => {
-    if (message.content === 'Wie is stom') {
-    	message.reply('Jij natuurlijk');
+    if (message.content === 'Kut') {
+    	message.reply('Niet schelden godverdomme');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === 'tering') {
+    	message.channel.send('Niet schelden godverdomme');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === 'hoi') {
+    	message.channel.send('doei');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === 'yo') {
+    	message.channel.send('hey homo');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === 'hey') {
+    	message.channel.send('doei');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === 'oke') {
+    	message.channel.send('vagina');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === 'nee') {
+    	message.channel.send('piemels likken');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === 'thx') {
+    	message.channel.send('niks thx zoek een leven');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === 'nice') {
+    	message.channel.send('wat nou nice lik een piemel');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === 'lol') {
+    	message.channel.send('je moeder is een snol');
   	}
 });
 
