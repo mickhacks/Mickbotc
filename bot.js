ffa48efc3bc550308f88652a73f396496d2e710a
mickhacks <mickbenjaminmulder@gmail.com>
2018-01-08 16:23:08 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -167,5 +167,11 @@ client.on('message', message => {
   	}
 });
 
+client.on('message', message => {
+    if (message.content === 'Yo') {
+    	message.reply('hey homo');
+  	}
+});
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
