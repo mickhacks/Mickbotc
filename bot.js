3f2edc7405c23038efb9b10b050f0cc43fbfec2e
mickhacks <mickbenjaminmulder@gmail.com>
2018-01-08 16:09:05 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,12 +2,8 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    console.log('I am ready!');
-    
-     let msg = message.content.toUpperCase(); 
-     let sender = message.author; 
-    
-});
+    console.log('I am ready!'); 
+    });
 
 client.on('message', message => {
     if (message.content === 'tyfus') {
